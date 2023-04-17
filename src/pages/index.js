0ae7f5958e1329a8ea4d7f1a453aa1388fcb5d43
jase-tech <103462247+jase-tech@users.noreply.github.com>
2023-04-17 17:39:21 +0300

    my code
    my code

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import HomeComponent from './Home'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <HomeComponent/>
+  )
+}
